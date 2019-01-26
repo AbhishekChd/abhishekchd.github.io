741c2c6d236f28e72f5fee9223a2e574e7e56830
Abhishek Chaudhary <acechd97@gmail.com>
2019-01-27 00:01:24 +0530

    Made function calls inline

--- js/script.js
+++ js/script.js
@@ -30,39 +30,31 @@ const PROJECTS_JSON = {
         }
     ]
 };
-const PROJECT_TEMPLATE =
-    `<div class="col-sm-10 col-md-4 col-lg-4 mb-5 offset-sm-1 offset-md-0">
-            <a target="_blank" class="silent-link"><div class="card mb-0" style="cursor: pointer; height: 100%">
-            <div class="card-header">
-            <h3></h3>
-                <h4 id="badges-holder"></h4>
-            </div>
-                <div class="flex-row"><img class="img-fluid d-block m-auto" style="height: 300px" alt="Card image" src=""></div>
-                <div class="card-body">
-                    <p class="card-text text-justify"></p>                                      
-                </div>  
-            </div>
-            </a>
-        </div>`;
 
-
-let renderItem = function (item) {
-    let card_template = $(PROJECT_TEMPLATE);
-    let badges = item.category.map(value => {
+function renderBadges(item) {
+    return item.category.map(value => {
         return `<span class="badge badge-primary badge-pill mr-2">${value}</span>`;
     });
+}
 
-    card_template.find("img").attr({
-        src: item.image,
-    });
-    card_template.find("a").attr({
-        href: item.link
-    });
-    card_template.find("p").html(item.description);
-    card_template.find("h3").html(item.name);
-    card_template.find("#badges-holder").html(badges);
-    return card_template;
-};
+function renderItem(item) {
+    return `<div class="col-sm-10 col-md-4 col-lg-4 mb-5 offset-sm-1 offset-md-0">
+            <a target="_blank" class="silent-link" href="${item.link}">
+                <div class="card mb-0" style="cursor: pointer; height: 100%">
+                    <div class="card-header">
+                        <h3>${item.name}</h3>
+                        <h4 id="badges-holder">${renderBadges(item)}</h4>
+                    </div>
+                    <div class="flex-row">
+                        <img class="img-fluid d-block m-auto" style="height: 300px" alt="Card image" src="${item.image}">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-justify">${item.description}</p>                                      
+                    </div>  
+                </div>
+            </a>
+        </div>`;
+}
 
 function showData() {
     let results = PROJECTS_JSON.projects.map(value => {
