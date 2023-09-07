946b1ba9ff28758a8c5db9a4d2fb2302b482c904
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-09-07 12:21:00 +0530

    Styled all hero components. Section complete! 🔥

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import * as Icon from "react-feather";
 
 const Hero = () => {
   const styles = {
     headerAccent: {
       color: "var(--color-primary)",
     },
+    connectLinksClasses:
+      "transition-all duration-150 ease-in-out underline underline-offset-8 hover:underline-offset-[14px]",
+    emailLinkClasses:
+      "transition-all duration-150 ease-in-out underline underline-offset-8 hover:underline-offset-[16px] decoration-8 decoration-[#6861b8]",
   };
 
   return (
@@ -21,9 +26,65 @@ const Hero = () => {
           </span>
         </h1>
         <div className="space-y-28">
-          <h3>Get in touch 👉 &nbsp; abhishek.chaudhary.in@gmail.com</h3>
+          <h3>
+            Get in touch 👉 <span className="m-3"></span>
+            <span className={styles.emailLinkClasses}>
+              <a
+                href="mailto:abhishek.chaudhary.in@gmail.com?subject=Connect from Portfolio!"
+                target="_blank"
+              >
+                abhishek.chaudhary.in@gmail.com
+              </a>
+            </span>
+          </h3>
 
-          <h4 className="py-8">Github / LinkedIn / Resume</h4>
+          <h4 className="py-8 flex items-center">
+            <div className={styles.connectLinksClasses}>
+              <a
+                href="https://github.com/AbhishekChd"
+                target="_blank"
+                className="flex gap-3"
+              >
+                <span className="mt-2">
+                  <Icon.GitHub />
+                </span>
+                <span>GitHub</span>
+              </a>
+            </div>
+
+            <span className="m-3">/</span>
+
+            <div className={styles.connectLinksClasses}>
+              <a
+                href="https://www.linkedin.com/in/abhishek-chd/"
+                target="_blank"
+                className="flex gap-3"
+              >
+                <span className="mt-1">
+                  <Icon.Linkedin
+                    fill={"var(--color-text)"}
+                    color="transparent"
+                  />
+                </span>
+                <span>LinkedIn</span>
+              </a>
+            </div>
+
+            <span className="m-3">/</span>
+
+            <div className={styles.connectLinksClasses}>
+              <a
+                href="https://drive.google.com/open?id=1F1TUqgsMxODNsPw83IuSw-DAvnNAHXCp"
+                target="_blank"
+                className="flex gap-3"
+              >
+                <span className="mt-2">
+                  <Icon.DownloadCloud />
+                </span>
+                <span>Resume</span>
+              </a>
+            </div>
+          </h4>
         </div>
       </section>
     </>
