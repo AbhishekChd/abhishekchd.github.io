9b6719b63788fe6ce6f68b5f82d31fc5f4c36f54
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-09-09 23:39:27 +0530

    Styled 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,23 @@
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import { HeadFC, PageProps } from "gatsby";
+import { SEO } from "../components/seo";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <section className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <h3 className="font-semibold header-accent">404</h3>
+        <h2 className="font-bold tracking-tighter text-gray-900">
+          Page not found
+        </h2>
+        <p className="mt-4 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for
+        </p>
+      </div>
+    </section>
   );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <SEO title="Not Found" />;
